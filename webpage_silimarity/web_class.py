0f7e93d07aa6ba0ad610e8119b9d203dd3fddb02
functionxu123 <1229853312@qq.com>
2018-07-13 00:11:01 +0800

    add input_names

--- webpage_silimarity/web_class.py
+++ webpage_silimarity/web_class.py
@@ -9,6 +9,7 @@ import re
 class Website_pages:
     def __init__(self,pagedir):
         self.fun_names=[]#stroe function names
+        self.input_names=[]#input name
         #self.
         
         self.feed(pagedir)
@@ -20,26 +21,39 @@ class Website_pages:
                 tepline=line.strip()
                 
                 self.fun_names.extend((self.getfun_names(tepline)))#add function names
+                self.input_names.extend((self.getinput_names(tepline)))
                 
-            
+                
+                
+    def getinput_names(self,instr):#<input name="tbox_pwd" type="password" id="tbox_pwd" class="dh_login_text" value="" />
+        restr=r'input.*?name\s*=[\"|\']*([\w|\_]+)[\"|\'|\s]*'
+        it=re.finditer(restr, instr)
+        return self.get_from_iter(it)
+        
+           
             
     def getfun_names(self,instr):#获取 function XXXX（）形式中的函数名：eg. function Clear() {
         restr=r'function\s+([\w|\_]+)\s*\('
-        retnames=[]
         
         it=re.finditer(restr, instr)
+        return self.get_from_iter(it)
+        
+        
+            
+    def get_from_iter(self,it,groupid=1):#从re的iter中取names
+        retnames=[]
         if it:
             for i in it:
                 #print i.group()
                 #self.fun_names.append(i.group(1).strip())
-                retnames.append(i.group(1).strip())
+                retnames.append(i.group(groupid).strip())
             
         #print retnames
         return retnames
-        
         
 
 if __name__ == '__main__':
     tep=Website_pages(u'F:/网络中心/网站相似度匹配/第一批首页/181.html')
     print tep.fun_names
+    print tep.input_names
     pass
