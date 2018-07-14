47bed59206e11e50f5ced2eea508d2588c20120b
functionxu123 <1229853312@qq.com>
2018-07-14 15:22:42 +0800

    add class id

--- webpage_silimarity/web_class.py
+++ webpage_silimarity/web_class.py
@@ -8,29 +8,41 @@ import re
 
 class Website_pages:
     def __init__(self,pagedir):
-        self.fun_names=[]#stroe function names
-        self.input_names=[]#input name
-        self.href_names=[]#to href
+        self.allnames={}
+        
+        self.allnames["fun_names"]=[]#stroe function names
+        self.allnames["input_names"]=[]#input name
+        self.allnames["href_names"]=[]#to href
+        self.allnames["id_class_names"]=[]
         #self.
         
         self.feed(pagedir)
         pass
     
+    def printout(self):
+        for i in self.allnames:
+            print i,":",self.allnames[i]
+            
+            
     def feed(self,pagedir):
         with open(pagedir,"r") as f:
             for line in f.readlines():
                 tepline=line.strip()
                 
-                self.fun_names.extend((self.getfun_names(tepline)))#add function names
-                self.input_names.extend((self.getinput_names(tepline)))
-                self.href_names.extend(self.gethref_name(tepline))#add href dir
+                self.allnames["fun_names"].extend((self.getfun_names(tepline)))#add function names
+                self.allnames["input_names"].extend((self.getinput_names(tepline)))
+                self.allnames["href_names"].extend(self.gethref_name(tepline))#add href dir
+                self.allnames["id_class_names"].extend(self.getclass_id_names(tepline))#id or class names
                 
                 
-                
-                self.fun_names=list(set(self.fun_names))
-                self.input_names=list(set(self.input_names))
-                self.href_names=list(set(self.href_names))
-                
+                for i in self.allnames:
+                    self.allnames[i]=list(set(self.allnames[i]))
+                '''
+                self.allnames["fun_names"]=list(set(self.allnames["fun_names"]))
+                self.allnames["input_names"]=list(set(self.allnames["input_names"]))
+                self.allnames["href_names"]=list(set(self.allnames["href_names"]))
+                self.allnames["id_class_names"]=list(set(self.allnames["id_class_names"]))
+                '''
                 
                 
     def getinput_names(self,instr):#<input name="tbox_pwd" type="password" id="tbox_pwd" class="dh_login_text" value="" />
@@ -64,6 +76,21 @@ class Website_pages:
             retstr.extend(tepre)
         
         return retstr
+    
+    def getclass_id_names(self,instr):#<div class="am-g" style="position: relative; top:60px">
+        #<div id="systitlecontainer">   <input id="loginid" type="text" class="loginipt" value="请输入用户名" />
+        # <td class="chklab">记住密码</td>
+        #<iframe id="ifrmRegister" frameborder="0" style="width:100%;height:100%;"></iframe>
+        restr=r'\b(id|class)\s*=\s*[\"|\']?([\w|\_|\s]+)[\"|\'|=]'
+        it=re.finditer(restr, instr, re.I)
+        teplist=self.get_from_iter(it,2)
+        
+        retlist=[]
+        for i in teplist:
+            retlist.extend(i.strip().split())
+            
+        return retlist
+    
         
         
             
@@ -77,13 +104,39 @@ class Website_pages:
             
         #print retnames
         return retnames
+    
+    
+    def compare_to(self,anoclass):
+        ret=[]
+        for i in self.allnames:
+            ret.append(self.overlap_rate(self.allnames[i], anoclass.allnames[i]))
+        return ret
+        
+    
+    def overlap_rate(self,lis1,lis2):#重叠度
+        overlap=0
+        for i in lis1:
+            if i in lis2:
+                overlap+=1
+        
+        return float(overlap)/(len(lis1)+len(lis2)-overlap)
+        
+    
+    
+    
+    
+    
         
 
 if __name__ == '__main__':
-    tep=Website_pages(u'F:/网络中心/网站相似度匹配/第一批首页/98.html')
-    print tep.fun_names
-    print tep.input_names
-    print tep.href_names
+    tep=Website_pages(u'E:\wokmaterial\emergencyCenter\第一批首页/102.html')
+    tep.printout()
+    
+    tep2=Website_pages(u'E:\wokmaterial\emergencyCenter\第一批首页/104.html')
+    tep2.printout()
+    
+    print tep.compare_to(tep2)
+    #print tep.overlap_rate(['aa','bdd','cd'], ['aa','bb','cc'])
     
     #print tep.gethref_name(r'<SCRIPT src="/js/comm/md5.js" type=text/javascript ></SCRIPT>')
     pass
