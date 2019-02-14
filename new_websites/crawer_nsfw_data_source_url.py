c40522e8c30a15208671470774f59e700451212e
root <root@sherl-desktop>
2019-02-14 21:17:45 +0800

    asdf

--- new_websites/crawer_nsfw_data_source_url.py
+++ new_websites/crawer_nsfw_data_source_url.py
@@ -8,7 +8,7 @@ import os
 import os.path as op
 import urllib2
 import requests
-import imghdr
+import imghdr,cv2
 from requests.adapters import HTTPAdapter
 
 req_sess = requests.Session()
@@ -45,13 +45,14 @@ examples:
 http://24.media.tumblr.com/b8192831bbbf40c64d9d9027aa092e02/tumblr_mi7btdQ4ui1rj6ivfo1_1280.jpg
 '''
 def imgfilecheck(fpath):
-    if not op.exists(fpath) or op.getsize(fpath)<=1024 or not imghdr.what(fpath):
+    if (not op.exists(fpath)) or op.getsize(fpath)<=1024 or ( cv2.imread(fpath) is None):
         if op.exists(fpath):os.remove(fpath)    #删除文件
+        #print 'judge error:',imghdr.what(fpath)
         return False
     return True
 
 def oneurl_download(url, outp, proxy_dict=None):
-    filename=op.split(url)[-1]
+    filename=op.split(url)[-1].split('?')[0]
     outfilename=op.join(outp, filename)
     
     if imgfilecheck(outfilename): 
@@ -69,7 +70,8 @@ def oneurl_download(url, outp, proxy_dict=None):
             print("Access Error when retrieve %s,code:%d." % (url, resp.status_code))
             raise Exception("Access Error")
         else:
-            print("Retrieve %s success." % (url))
+            print("Connect %s success." % (url))
+            print 'writing to ',outfilename
         
             
             
@@ -80,6 +82,7 @@ def oneurl_download(url, outp, proxy_dict=None):
     except :
         # try again
         if not proxy_dict:
+            print 'trying use proxy ...'
             return oneurl_download(url, outp, proxy_dict=proxy_dict)
         else:
             return False   
@@ -92,19 +95,26 @@ def one_txt_download(txtpath, outp):
         freadl=f.readlines()
         l=len(freadl)
         for ind,i in enumerate(freadl):
+            print ind,'/',l
             tepi=i.strip()
             res=oneurl_download(tepi, outp)
-            print ind,'/',l
+            
             if res:
                 cnt_true+=1
                 print 'download ',i,' success'
+            else:
+                print 'download ',i,' failed'
     print 'download ',txtpath,' done-->',cnt_true,'/',l
     return cnt_true,l
 
-
+def main():
+    paths=getalltxtpath_and_buildoutputpath(inpath , outpath)
+    for ind,i in enumerate(paths):
+        print 'txt file cnts:',ind,'/',len(paths)
+        one_txt_download(*i)
 
 if __name__ == '__main__':
-    pass
+    main()
 
 
 
