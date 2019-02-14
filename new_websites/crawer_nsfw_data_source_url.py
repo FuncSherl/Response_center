4c039f2a6ee970b860d63b99ae853209cc024ce7
root <root@sherl-desktop>
2019-02-14 20:52:40 +0800

    add nsfw crawer

---
+++ new_websites/crawer_nsfw_data_source_url.py
@@ -0,0 +1,112 @@
+#coding=utf-8
+'''
+Created on Feb 14, 2019
+
+@author: root
+'''
+import os
+import os.path as op
+import urllib2
+import requests
+import imghdr
+from requests.adapters import HTTPAdapter
+
+req_sess = requests.Session()
+req_sess.mount('http://', HTTPAdapter(max_retries=3))
+req_sess.mount('https://', HTTPAdapter(max_retries=3))
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+inpath=r'/home/sherl/git/nsfw_data_source_urls/raw_data'
+outpath=r'/media/sherl/本地磁盘1/data_DL/nsfw_data_source_imgs'
+
+headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+proxy_dict = {'http': 'socks5://127.0.0.1:1080','https':'socks5://127.0.0.1:1080'}#use local socket5 proxy,with shadowshocks
+TIMEOUT=(20,30)
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+def getalltxtpath_and_buildoutputpath(ind, outd):
+    ret=[]
+    for i in os.listdir(ind):
+        tepin=op.join(ind, i)
+        tepout=op.join(outd, i)
+        
+        if op.isdir(tepin):
+            if not op.exists(tepout): os.makedirs(tepout)
+            ret+=getalltxtpath_and_buildoutputpath(tepin, tepout)
+        elif op.splitext(i)[-1] in ['.txt']:
+            ret.append([tepin,outd])
+    return ret
+            
+#paths=getalltxtpath_and_buildoutputpath(inpath , outpath)
+#print paths[:5]
+'''
+examples:
+http://24.media.tumblr.com/b8192831bbbf40c64d9d9027aa092e02/tumblr_mi7btdQ4ui1rj6ivfo1_1280.jpg
+'''
+def imgfilecheck(fpath):
+    if not op.exists(fpath) or op.getsize(fpath)<=1024 or not imghdr.what(fpath):
+        if op.exists(fpath):os.remove(fpath)    #删除文件
+        return False
+    return True
+
+def oneurl_download(url, outp, proxy_dict=None):
+    filename=op.split(url)[-1]
+    outfilename=op.join(outp, filename)
+    
+    if imgfilecheck(outfilename): 
+        return True
+    
+    try:
+        resp = req_sess.get(url,
+                            stream=True,
+                            proxies=proxy_dict,
+                            headers=headers,
+                            verify=False,
+                            timeout=TIMEOUT)
+        
+        if resp.status_code !=requests.codes.ok:
+            print("Access Error when retrieve %s,code:%d." % (url, resp.status_code))
+            raise Exception("Access Error")
+        else:
+            print("Retrieve %s success." % (url))
+        
+            
+            
+        with open(outfilename, 'wb') as fh:
+            for chunk in resp.iter_content(chunk_size=1024):
+                fh.write(chunk)
+                
+    except :
+        # try again
+        if not proxy_dict:
+            return oneurl_download(url, outp, proxy_dict=proxy_dict)
+        else:
+            return False   
+    return imgfilecheck(outfilename)
+
+
+def one_txt_download(txtpath, outp):
+    cnt_true=0
+    with open(txtpath,'r') as f:
+        freadl=f.readlines()
+        l=len(freadl)
+        for ind,i in enumerate(freadl):
+            tepi=i.strip()
+            res=oneurl_download(tepi, outp)
+            print ind,'/',l
+            if res:
+                cnt_true+=1
+                print 'download ',i,' success'
+    print 'download ',txtpath,' done-->',cnt_true,'/',l
+    return cnt_true,l
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
