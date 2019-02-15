3d55482c0385f5f9bea1e946b7df784d53f141b5
root <root@sherl-desktop>
2019-02-15 11:10:40 +0800

    asd

--- new_websites/crawer_nsfw_data_source_url.py
+++ new_websites/crawer_nsfw_data_source_url.py
@@ -18,7 +18,7 @@ req_sess.mount('https://', HTTPAdapter(max_retries=3))
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 inpath=r'/home/sherl/git/nsfw_data_source_urls/raw_data'
 outpath=r'/media/sherl/本地磁盘/data_DL/nsfw_data_source_imgs'
-thread_maxcnt=8
+thread_maxcnt=6
 
 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 proxy_dict_ss = {'http': 'socks5://127.0.0.1:1080','https':'socks5://127.0.0.1:1080'}#use local socket5 proxy,with shadowshocks
