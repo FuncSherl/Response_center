16328b2bc5e1e545b178836598f7d7d5b5589007
functionxu123 <1229853312@qq.com>
2019-02-26 12:19:16 +0800

    to find errors

--- new_websites/crawer_nsfw_data_source_url.py
+++ new_websites/crawer_nsfw_data_source_url.py
@@ -23,7 +23,7 @@ elif pc_id==1:
     outpath=r'/media/sherl/本地磁盘/data_DL/nsfw_data_source_imgs'
 
 thread_maxcnt=1 #1 是为了找到错误原因
-start_cnt=300
+start_cnt=300  #从哪个txt开始，这个是为了尽快找到错误原因
 
 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 proxy_dict_ss = {'http': 'socks5://127.0.0.1:1080','https':'socks5://127.0.0.1:1080'}#use local socket5 proxy,with shadowshocks
